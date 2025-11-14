e6880e46a5f79bdc8845fb61e8d3361486b5ea5c
Sangam Puri <sangampuree@gmail.com>
2025-11-14 18:03:25 +0545

    Conditional statementfinished

---
+++ day4part2.py
@@ -0,0 +1,34 @@
+
+#!conditional statements in python
+
+# number = int(input("Enter a number: "))
+# if number>0:
+#     print(f"{number} is a positive number.")
+# print("A statement outside the if statement")
+
+# x=8
+# total = 0
+# if x != 0:
+#     total +=x
+#     print(total)
+
+# print("This is always executed")
+
+# number = int(input("Enter a number :"))
+# if number>0:
+#     print(f"{number} is positive")
+# else:
+#     print(f"{number} is negative")
+
+# number = -5
+
+# if number > 0:
+#     print('Positive number')
+
+# elif number < 0:
+#     print('Negative number')
+
+# else:
+#     print('Zero')
+
+# print('This statement is always executed')
