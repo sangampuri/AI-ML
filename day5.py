855a6bd0964a545f1d21bf448a3b6621ddfe5003
Sangam Puri <sangampuree@gmail.com>
2025-11-16 09:33:20 +0545

    for and while loop completed

---
+++ day5.py
@@ -0,0 +1,43 @@
+# primes = [2,3,5,7]
+# for prime in primes:
+#     print(prime)
+
+# for x in range(5):
+#     print(x)
+
+# for x in range (3,11):
+#     print(x)
+
+# for x in reversed(range(11)):
+#     print(x)
+
+# print("Happy New Year")
+
+# credit_card = "1234-5678-9123"
+# for x in credit_card:
+#     print(x)
+
+# for x in range(1,20):
+#     if x==12:
+#         continue
+#     else:
+#         print(x)
+
+# count = 0
+# while (count<=5):
+#     print(count)
+#     count +=1
+
+# count =0
+# while True:
+#     print(count)
+#     count+=1
+#     if(count>=5):
+#         break
+
+count=0
+while(count<5):
+    print(count)
+    count +=1
+else:
+    print(f"count value reached  {count}")
