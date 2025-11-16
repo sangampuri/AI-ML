4a2b546d4276c45af6502d9e74cddf9e7bb70f78
Sangam Puri <sangampuree@gmail.com>
2025-11-16 12:16:13 +0545

    Functions finished

---
+++ day5part2.py
@@ -0,0 +1,33 @@
+#functions 
+
+# def fun():
+#     print("You have created a function")
+
+# fun()
+    
+# def greeting(name):
+#     print(f"Namaste!{name}")
+
+# greeting("sangam")    
+
+# def sum(a,b):
+#     return a+b
+
+# result=sum(5,5)
+# print(result)
+
+# def list_benefits():
+#     print(["More organized code", "More readable code", "Easier code reuse", "Allowing programmers to share and connect code together"])
+
+# list_benefits()
+
+# def celsius_to_ferenheit(celsius):
+#     ferenheit = (celsius * 9 / 5) +32
+#     return ferenheit
+
+# def main():
+#   celsius=float(input("Enter the tempertature in celsius:"))
+#   result = celsius_to_ferenheit(celsius)
+#   print(result)
+
+# main()
